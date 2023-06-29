f6d0ef1631093b98955fbdf0f101a17c806363f3
max fabin <maxikfabin@gmail.com>
2023-06-29 23:30:01 +0300

    .

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
     // baseUrl:'http://localhost:5001',
-    baseUrl:"https://spacerateserver-production.up.railway.app",
+    baseUrl:"https://api.goodjoy.uz",
     prepareHeaders: (headers) =>{
         const token = localStorage.getItem('token')
         if(!!token) headers.set('authorization', `Bearer ${token}`)
