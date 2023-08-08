80ea9ad69bf05030b2c5f28e3eb887fba1d512a2
max fabin <maxikfabin@gmail.com>
2023-08-08 23:58:44 +0300

    jwt

--- src/components/AuthRequire.jsx
+++ src/components/AuthRequire.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {useAuth} from "../utils.js";
 import {Navigate, Outlet} from "react-router";
 
 const AuthRequire = () => {
     const token = localStorage.getItem('token')
+    console.log(token)
     return (
         !!token ? <Outlet/> : <Navigate to={'/login'} />
     );
